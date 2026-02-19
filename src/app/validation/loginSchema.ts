2b68860be26a1340c0b78e581ed31c625d60c6bb
shashikesh234 <uitshashikesh@gmail.com>
2026-02-19 15:47:24 +0530

    feat: login page created

---
+++ src/app/validation/loginSchema.ts
@@ -0,0 +1,13 @@
+import * as yup from "yup";
+
+export const loginValidationSchema = yup.object({
+  email: yup
+    .string()
+    .email("Invalid email format")
+    .required("Email is required"),
+
+  password: yup
+    .string()
+    .min(6, "Password must be at least 6 characters")
+    .required("Password is required"),
+});
